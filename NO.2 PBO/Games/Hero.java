31c37d52db48a92cf9b9f933ccd054d7a12f99c3
Tirawati <114417749+Tirawati@users.noreply.github.com>
2022-12-23 15:31:47 +0700

    Add files via upload

---
+++ NO.2 PBO/Games/Hero.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tirawati_20210040078_TI21F
+ */
+public abstract class Hero {
+    abstract String getName();
+    abstract float getHp();
+    abstract float getAttack();
+    abstract float getDefense();
+    public abstract void infoHero();
+}
---
+++ NO.2 PBO/Games/Main.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tirawati_20210040078_TI21F
+ */
+
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) {
+        Tank tigril=new Tank("Tigril");
+        Tank franco=new Tank("Franco",2700,120,25);
+        Tank akai=new Tank("Akai",2800,110,20);
+
+        Marksman clint=new Marksman("Clint");
+        Marksman lesley=new Marksman("Lesley",2400,150,14);
+        Marksman bruno=new Marksman("Bruno",2500,140,17);
+
+        ArrayDeque<Marksman> heroMarksman=new ArrayDeque<Marksman>();
+        ArrayDeque<Tank> heroTank=new ArrayDeque<Tank>();
+
+        heroTank.add(tigril);
+        heroTank.addFirst(franco);
+        heroTank.add(akai);
+
+        heroMarksman.add(clint);
+        heroMarksman.add(lesley);
+        heroMarksman.addFirst(bruno);
+
+        System.out.println("Hero-hero Tank : ");
+        for(Tank tank: heroTank){
+            tank.infoHero();
+        }
+        System.out.println("");
+        System.out.println("Hero-hero Marksman :");
+        for(Marksman mm: heroMarksman){
+            mm.infoHero();
+        }
+    }
+}
